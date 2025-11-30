5f9ca1f4dc01521c2f4f612f95bf114b46e38a25
uesan <machine_slalom.2h@icloud.com>
2025-11-30 16:33:19 +0900

    mvvを追加しました

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,89 @@
 import Image from "next/image";
+import building from "@/assets/mvv/building.png";
+import presentation from "@/assets/mvv/presentation.jpg";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        {/* mvv */}
+        <div className="h-[1220px] w-full bg-secondary flex flex-col items-center">
+          <Image
+            src={building}
+            alt="背景の建物"
+            className="w-full h-auto block relative top-0 m-0"
+          />
+
+          <div className="h-[1076px] w-[90%] bg-card rounded-[70px_0_0_70px] p-[80px_10px_80px_100px] box-border -mt-3.5 ml-auto relative">
+            <div className="w-[50%] aspect-100/76 overflow-hidden absolute top-1/3 right-0 z-1">
+              <Image
+                src={presentation}
+                alt="プレゼン画像"
+                className="w-full h-full object-cover object-[right_center] opacity-20"
+              />
+              <div className="absolute inset-0 bg-linear-to-r from-white to-transparent"></div>
+            </div>
+
+            <div className="relative z-2">
+              <h3 className="mt-4 mb-4 font-bold text-[32px]">
+                私たちは「MVV」を掲げています。
+              </h3>
+              <h4 className="mt-5 mb-5 font-bold text-[24px]">
+                あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
+              </h4>
+            </div>
+
+            <div className="relative z-2">
+              <div>
+                <h2 className="mt-3 mb-3 font-bold flex items-end">
+                  <span className="text-[100px] leading-none">M</span>
+                  <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
+                    ission
+                  </span>
+                </h2>
+                <p className="leading-relaxed relative mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
+                  ミッション
+                </p>
+                <p className="leading-relaxed mt-4 mb-4 text-[16px] font-bold">
+                  あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
+                </p>
+              </div>
+
+              <div className="ml-[10vw]">
+                <h2 className="font-bold mt-3 mb-3 flex items-end">
+                  <span className="text-[100px] leading-none">V</span>
+                  <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
+                    ision
+                  </span>
+                </h2>
+                <p className="leading-relaxed relative mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
+                  ビジョン
+                </p>
+                <p className="leading-relaxed mt-4 mb-4 text-[16px] font-bold">
+                  あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
+                </p>
+              </div>
+
+              <div className="ml-[20vw]">
+                <h2 className="font-bold mt-3 mb-3 flex items-end">
+                  <span className="text-[100px] leading-none">V</span>
+                  <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
+                    alue
+                  </span>
+                </h2>
+                <p className="relative leading-relaxed mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
+                  バリュー
+                </p>
+                <p className="leading-relaxed mt-4 mb-4 text-[16px] font-bold">
+                  あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/*mvvここまで */}
+
         <Image
           className="dark:invert"
           src="/next.svg"
