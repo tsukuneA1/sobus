4afb1ab959e96a3267e23b8d6b311db5f54c5572
tsukuneA1 <tkschr1125@gmail.com>
2025-12-06 00:43:32 +0900

    fix:semantics

--- app/src/app/_components/mvv-section.tsx
+++ app/src/app/_components/mvv-section.tsx
@@ -11,7 +11,7 @@ export const MvvSection = () => {
         className="w-full h-auto block relative top-0 m-0"
       />
 
-      <article className="h-[1076px] w-[90%] bg-card rounded-[70px_0_0_70px] p-[80px_10px_80px_100px] box-border -mt-3.5 ml-auto relative">
+      <div className="h-[1076px] w-[90%] bg-card rounded-[70px_0_0_70px] p-[80px_10px_80px_100px] box-border -mt-3.5 ml-auto relative">
         <figure className="w-[50%] aspect-100/76 overflow-hidden absolute top-1/3 right-0 z-1">
           <Image
             src={presentation}
@@ -21,38 +21,38 @@ export const MvvSection = () => {
           <div className="absolute inset-0 bg-linear-to-r from-white to-transparent"></div>
         </figure>
 
-        <div className="relative z-2">
-          <h3 className="mt-4 mb-4 font-bold text-[32px]">
+        <header className="relative z-2">
+          <h2 className="mt-4 mb-4 font-bold text-[32px]">
             私たちは「MVV」を掲げています。
-          </h3>
-          <h4 className="mt-5 mb-5 font-bold text-[24px]">
+          </h2>
+          <p className="mt-5 mb-5 font-bold text-[24px]">
             あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、
-          </h4>
-        </div>
+          </p>
+        </header>
 
         <div className="relative z-2">
-          <section>
-            <h2 className="mt-3 mb-3 font-bold flex items-end">
+          <div>
+            <h3 className="mt-3 mb-3 font-bold flex items-end">
               <span className="text-[100px] leading-none">M</span>
               <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
                 ission
               </span>
-            </h2>
+            </h3>
             <p className="leading-relaxed relative mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
               ミッション
             </p>
             <p className="leading-relaxed mt-4 mb-4 text-[16px] font-bold">
               社会課題とビジネスの架け橋になる
             </p>
-          </section>
+          </div>
 
-          <section className="ml-[10vw]">
-            <h2 className="font-bold mt-3 mb-3 flex items-end">
+          <div className="ml-[10vw]">
+            <h3 className="font-bold mt-3 mb-3 flex items-end">
               <span className="text-[100px] leading-none">V</span>
               <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
                 ision
               </span>
-            </h2>
+            </h3>
             <p className="leading-relaxed relative mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
               ビジョン
             </p>
@@ -60,24 +60,24 @@ export const MvvSection = () => {
               ボランティアをする学生/ビジネスに関心のある学生が現場で体感・実践すると共に、
               「ソーシャルビジネス」を大学生に伝えていく
             </p>
-          </section>
+          </div>
 
-          <section className="ml-[20vw]">
-            <h2 className="font-bold mt-3 mb-3 flex items-end">
+          <div className="ml-[20vw]">
+            <h3 className="font-bold mt-3 mb-3 flex items-end">
               <span className="text-[100px] leading-none">V</span>
               <span className="text-[80px] leading-none bg-linear-to-r from-accent-green to-primary bg-clip-text text-transparent opacity-60">
                 alue
               </span>
-            </h2>
+            </h3>
             <p className="relative leading-relaxed mt-4 mb-4 text-[24px] font-bold after:content-[''] after:absolute after:left-0 after:bottom-[-5px] after:w-[197px] after:h-1 after:bg-linear-to-r after:from-primary after:to-secondary">
               バリュー
             </p>
             <p className="leading-relaxed mt-4 mb-4 text-[16px] font-bold">
               やりたいことをやる・人を巻き込んで環境を変える
             </p>
-          </section>
+          </div>
         </div>
-      </article>
+      </div>
     </section>
   );
 };
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,7 +3,7 @@ import { MvvSection } from "./_components/mvv-section";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32  bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start">
         <MvvSection />
       </main>
     </div>
