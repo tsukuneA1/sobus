1d7f6ca8510425b92cd98bb2d4274c276413b596
tsukuneA1 <tkschr1125@gmail.com>
2025-12-06 00:40:21 +0900

    fmt

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col items-center justify-between py-32  bg-white dark:bg-black sm:items-start">
-         <MvvSection />
+        <MvvSection />
       </main>
     </div>
   );
