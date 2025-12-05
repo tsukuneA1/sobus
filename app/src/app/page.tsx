48f4c99f1b86f49368a20e4f325ee6175056f9c6
tsukuneA1 <tkschr1125@gmail.com>
2025-12-06 01:15:10 +0900

    fix:style

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,10 +1,13 @@
+import { GreetingSection } from "./_components/greeting-section";
 import { MvvSection } from "./_components/mvv-section";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full flex-col items-center justify-between bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full flex-col gap-[105px] items-center justify-between dark:bg-black sm:items-start bg-secondary">
         <MvvSection />
+
+        <GreetingSection />
       </main>
     </div>
   );
