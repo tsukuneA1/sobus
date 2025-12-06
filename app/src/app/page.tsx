3e4cab11bfca9c011d34079533ef7abf5facb3a2
tsukuneA1 <tkschr1125@gmail.com>
2025-12-06 10:02:46 +0900

    draft

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,14 @@
 import { GreetingSection } from "./_components/greeting-section";
 import { MvvSection } from "./_components/mvv-section";
+import { ActivityImagesSection } from "./_components/activity-images-section";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full flex-col gap-[105px] items-center justify-between dark:bg-black sm:items-start bg-secondary">
         <MvvSection />
+
+        <ActivityImagesSection />
 
         <GreetingSection />
       </main>
