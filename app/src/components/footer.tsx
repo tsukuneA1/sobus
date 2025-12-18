2856eae61f625a44dc17b0984d638591bb041a2f
tsukuneA1 <tkschr1125@gmail.com>
2025-12-18 19:08:40 +0900

    responsive:footer

--- app/src/components/footer.tsx
+++ app/src/components/footer.tsx
@@ -4,43 +4,37 @@ import { GradientLink } from "./gradient-link";
 
 export const Footer = () => {
   return (
-    <footer className="bg-secondary/50 backdrop-blur supports-[backdrop-filter]:bg-secondary/50 text-foreground">
-      <div className="mx-auto max-w-screen-xl px-6 py-10 md:pt-[50px] md:pb-[110px]">
-        <div className="grid grid-cols-1 items-center gap-10 md:grid-cols-2">
-          <div className="space-y-4">
-            <p className="text-base font-medium leading-normal text-black">
-              ソーシャルビジネス起業プロジェクト・ソービズ
-            </p>
-            <p className="text-base font-medium leading-normal text-black mb-[50px]">
-              毎週水曜日活動
-            </p>
+    <footer className="flex flex-col md:flex-row md:justify-between md:items-center py-[50px] px-[25px] md:px-[150px] md:pb-[110px] bg-secondary/50 text-foreground">
+      <div>
+        <p className="text-base font-medium leading-normal">
+          ソーシャルビジネス起業プロジェクト・ソービズ
+        </p>
+        <p className="text-base font-medium leading-normal mt-2 mb-[50px]">
+          毎週水曜日活動
+        </p>
 
-            <div className="mt-4 flex w-full max-w-[225px] flex-col gap-3">
-              <GradientLink
-                href="https://www.instagram.com/wavoc_social_business_/"
-                text="Instagram"
-                target="_blank"
-              />
-              <GradientLink
-                href="https://x.com/wavoc_sbp"
-                text="X"
-                target="_blank"
-              />
-            </div>
-          </div>
-
-          <div className="flex justify-end">
-            <Image
-              src={FooterLogo}
-              alt="ソービズ ロゴ"
-              width={420}
-              height={160}
-              className="h-auto w-[260px] md:w-[380px] lg:w-[420px]"
-              priority={false}
-            />
-          </div>
+        <div className="mt-4 flex w-full max-w-[225px] flex-col gap-6">
+          <GradientLink
+            href="https://www.instagram.com/wavoc_social_business_/"
+            text="Instagram"
+            target="_blank"
+          />
+          <GradientLink
+            href="https://x.com/wavoc_sbp"
+            text="X"
+            target="_blank"
+          />
         </div>
       </div>
+
+      <Image
+        src={FooterLogo}
+        alt="ソービズ ロゴ"
+        width={240}
+        height={87}
+        className="h-auto w-[240px] md:w-[350px] mt-[88px] md:mt-0 self-end md:self-auto"
+        priority={false}
+      />
     </footer>
   );
 };
