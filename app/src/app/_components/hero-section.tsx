cc65fa088753f4e6137e236dd11890c4d29a7f79
Shiori572 <shiori5721154@gmail.com>
2025-12-18 23:07:15 +0900

    responsive

--- app/src/app/_components/hero-section.tsx
+++ app/src/app/_components/hero-section.tsx
@@ -1,25 +1,26 @@
 import Image from "next/image";
 import Toplogo from "@/assets/hero-section/sobus-logo.png";
 import TopBridge from "@/assets/hero-section/top-bridge.png";
+import TopBridgeResponsive from "@/assets/hero-section/top-bridge-responsive.png";
 import TopPhoto from "@/assets/hero-section/top-photo.png";
 
 export const HeroSection = () => {
   return (
-    <div className="flex flex-col relative h-[800px]">
-      <h2 className="text-[50px] font-bold tracking-[8] text-[#EB8338CC] ml-30 mt-5">
+    <div className="h-[670px] flex flex-col relative md:h-[800px]">
+      <h2 className="text-[20px] ml-[18px] mt-[58px] tracking-[0.1rem] font-bold  text-[#EB8338CC]  md:ml-32 md:mt-5 md:text-[50px] md:tracking-[0.5rem] ">
         WASEDA UNIVERCITY
       </h2>
       <div className="flex relative justify-end">
         <Image
-          className=" w-[75vw] aspect-[1.7] object-cover opacity-[0.6] "
+          className="w-[80vw] h-[333px] object-cover opacity-60 md:w-[75vw] md:aspect-[1.7] z-10"
           src={TopPhoto}
           alt="top-photo"
         />
       </div>
-      <h1 className=" absolute text-[116px] tracking-[8] font-bold text-white top-15 left-25">
+      <h1 className=" absolute text-[50px]  ml-[18px] mt-[70px] tracking-[8] font-bold text-white md:top-20 md:left-25 md:text-[116px] z-10">
         SOCIAL<br></br>BUSINESS<br></br>CIRCLE
       </h1>
-      <div className="absolute top-120 ">
+      <div className="absolute top-120 hidden md:block">
         <Image
           className="flex"
           src={TopBridge}
@@ -28,9 +29,18 @@ export const HeroSection = () => {
           height={340}
         />
       </div>
+      <div className="absolute top-[350px] block md:hidden z-0 ">
+        <Image
+          className="flex"
+          src={TopBridgeResponsive}
+          alt="top-bridges"
+          width={570}
+          height={267}
+        />
+      </div>
 
       <Image
-        className="h-auto absolute bottom-0 right-10"
+        className="w-[221px] h-auto absolute right-[25px] bottom-0 md:right-10 md:w-[421px]"
         src={Toplogo}
         alt="sobus-logo"
         width={421}
