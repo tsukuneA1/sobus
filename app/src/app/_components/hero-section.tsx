c640e3e389d0c178d873fd4f2d7f4c5ae132e6fd
Shiori572 <shiori5721154@gmail.com>
2025-12-20 00:30:30 +0900

    responsive

--- app/src/app/_components/hero-section.tsx
+++ app/src/app/_components/hero-section.tsx
@@ -6,40 +6,30 @@ import TopPhoto from "@/assets/hero-section/top-photo.png";
 
 export const HeroSection = () => {
   return (
-    <div className="h-[670px] flex flex-col relative md:h-[800px]">
+    <section className="h-[670px] flex flex-col relative md:h-[800px]">
       <h2 className="text-[20px] ml-[18px] mt-[58px] tracking-[0.1rem] font-bold  text-[#EB8338CC]  md:ml-32 md:mt-5 md:text-[50px] md:tracking-[0.5rem] ">
         WASEDA UNIVERCITY
       </h2>
-      <div className="flex relative justify-end">
+      <div className="flex justify-end">
         <Image
-          className="w-[80vw] h-[333px] object-cover opacity-60 md:w-[75vw] md:aspect-[1.7] z-10"
+          className="absolute w-[80vw] h-[333px] object-cover opacity-60 md:w-[75vw] md:h-[634px] md:aspect-[1.7] "
           src={TopPhoto}
           alt="top-photo"
         />
       </div>
-      <h1 className=" absolute text-[50px]  ml-[18px] mt-[70px] tracking-[8] font-bold text-white md:top-20 md:left-25 md:text-[116px] z-10">
+      <h1 className=" absolute text-[50px]  ml-[18px] mt-[70px] tracking-[8] font-bold text-white md:top-20 md:left-25 md:text-[116px] ">
         SOCIAL<br></br>BUSINESS<br></br>CIRCLE
       </h1>
-      <div className="absolute top-120 hidden md:block">
+   
         <Image
-          className="flex"
+          className="absolute top-90 h-[267px]  md:h-[340px] md:w-[1200px] md:top-120"
           src={TopBridge}
           alt="top-bridges"
           width={1000}
           height={340}
+          
         />
-      </div>
-      <div className="absolute top-[350px] block md:hidden z-0 ">
-        <Image
-          className="flex"
-          src={TopBridgeResponsive}
-          alt="top-bridges"
-          width={570}
-          height={267}
-        />
-      </div>
-
-      <Image
+       <Image
         className="w-[221px] h-auto absolute right-[25px] bottom-0 md:right-10 md:w-[421px]"
         src={Toplogo}
         alt="sobus-logo"
@@ -47,6 +37,6 @@ export const HeroSection = () => {
         height={203}
         priority={true}
       />
-    </div>
+    </section>
   );
 };
